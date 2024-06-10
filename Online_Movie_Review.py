a1cb99b3d9644bc30a8a4ad4df4ccb12ba68d097
Dishapurohit <36095841+Dishapurohit@users.noreply.github.com>
2024-06-10 16:22:13 +0530

    Update Online_Movie_Review.py

--- Online_Movie_Review.py
+++ Online_Movie_Review.py
@@ -1,32 +1,3 @@
-# Capstone Project
-# Overview
-# If you are planning on going out to see a movie, how well can you trust online reviews and ratings?
-# Especially if the same company showing the rating also makes money by selling movie tickets.
-# Do they have a bias towards rating movies higher than they should be rated?
-
-# Goal:
-# Your goal is to complete the tasks below based off the 538 article and see if you reach a
-# similar conclusion. You will need to use your pandas and visualization skills to determine if
-# Fandango's ratings in 2015 had a bias towards rating movies better to sell more tickets.
-
-# Complete the tasks written in bold.
-
-# Part One: Understanding the Background and Data
-# The Data
-# This is the data behind the story Be Suspicious Of Online Movie Ratings, Especially Fandango’s
-# openly available on 538's github: https://github.com/fivethirtyeight/data.
-# There are two csv files, one with Fandango Stars and Displayed Ratings,
-# and the other with aggregate data for movie ratings from other sites, like Metacritic,IMDB,
-# and Rotten Tomatoes.
-
-# all_sites_scores.csv
-# all_sites_scores.csv contains every film that has a Rotten Tomatoes rating, a RT User rating,
-# a Metacritic score, a Metacritic User score, and IMDb score, and at least 30 fan reviews on Fandango.
-# The data from Fandango was pulled on Aug. 24, 2015.
-
-# fandango_scape.csv
-# fandango_scrape.csv contains every film 538 pulled from Fandango.
-
 # TASK: Import any libraries you think you will use:
 import numpy as np
 import pandas as pd
